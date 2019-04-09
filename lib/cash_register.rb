20cd3699b9f3d0bacacb20d1085b06bb9b92925e
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-09 20:23:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+require 'pry'
 
+class CashRegister 
+  
+  attr_accessor :total, :discount, :price, :items 
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount 
+    @items = []
+  end
+  
+  def add_item(item, price, quantity = 1)
+    @price = price 
+    @item = item 
+    
+    
+    #binding.pry 
+    
+  end
+  
+  
+  
+end
